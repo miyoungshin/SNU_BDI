2b122baf26609011c7c3263b6c2a0681d8809e98
arnorfati <30887534+arnorfati@users.noreply.github.com>
2017-09-17 22:49:37 +0900

    20170917_update

--- 탐색적분석_시각화/탐색적_과제_skillcraft.R
+++ 탐색적분석_시각화/탐색적_과제_skillcraft.R
@@ -1,62 +1,60 @@
-# 데이터 불러오기1
+# 데이터 & library 불러오기
 
 setwd('C:/Users/User/Documents/GitHub/SNU_BDI/탐색적분석_시각화')
 setwd('/Users/arnorfati/Documents/GitHub/SNU_BDI/탐색적분석_시각화')
 skilldata = read.csv('SkillCraft1_Dataset.csv',stringsAsFactors = F, na.strings = c('?'))
-colSums(is.na(skilldata))
 library(dplyr)                    
+library(colorspace)
 
-# 전처리
-
-
-#---------------------------------------------------------------------#
-# skilldata
+# 데이터 파악
 summary(skilldata)
+dim(skilldata)
+str(skilldata)
+colSums(is.na(skilldata))
+attach(skilldata)
+hist(LeagueIndex,breaks = 8)
+
+# 등급 별 데이터 분류
+skilldata_1 = skilldata[skilldata$LeagueIndex == 1,]
+skilldata_2 = skilldata[skilldata$LeagueIndex == 2,]
+skilldata_3 = skilldata[skilldata$LeagueIndex == 3,]
+skilldata_4 = skilldata[skilldata$LeagueIndex == 4,]
+skilldata_5 = skilldata[skilldata$LeagueIndex == 5,]
+skilldata_6 = skilldata[skilldata$LeagueIndex == 6,]
+skilldata_7 = skilldata[skilldata$LeagueIndex == 7,]
+skilldata_8 = skilldata[skilldata$LeagueIndex == 8,]
+
+# 시각화에 활용할 색상
+mycol = c('saddle brown','Azure 3','gold2','AQUAMARINE','plum','light sky blue',' royal blue','midnight blue')
+
+
+# 예측변수인 'LEagueIndex' 전처리 & 시각화
 table_League = table(skilldata$LeagueIndex)
 table.name = c('Bronze','Silver','Gold','Platinum','Diamond','Master','GrandMaster','Pro')
 bar.a = barplot(table_League,ylim = c(0,1000), main = 'LeagueIndex',names.arg = table.name,col = c('saddle brown','Azure 3','gold2','AQUAMARINE','plum','light sky blue',' royal blue','midnight blue'))
 legend('topright',c('Bronze','Silver','Gold','Platinum','Diamond','Master','GrandMaster','Pro'),col = c('saddle brown','Azure 3','gold2','AQUAMARINE','plum','light sky blue',' royal blue','midnight blue'),pch=15)
 text(bar.a,cex=1.7,table_League+100, labels = paste(round(table_League, 2),"", sep=''))
-mycol = c('saddle brown','Azure 3','gold2','AQUAMARINE','plum','light sky blue',' royal blue','midnight blue')
-mycol = colors()
-plot(1:80, y=rep(1,80), col = mycol[1:80], cex = 2, pch = 20, 
-     ylim = c(0,1) )
-points(1:80, y=rep(0.5,80), col = mycol[81:160], cex = 2, pch = 20 )
-points(1:80, y=rep(0,80), col = mycol[161:240], cex = 2, pch = 20 )
-install.packages('colorspace')
-library(colorspace)
-pal = choose_palette()
-
-str(skilldata)
-skilldata$LeagueIndex = factor(skilldata$LeagueIndex,ordered = TRUE)
-str
-detach(skilldata)
-attach(skilldata)
-summary(skilldata)
-hist(LeagueIndex,breaks = 8)
-pie(LeagueIndex)
-boxplot(Age) # 이상값 다수 존재 
-hist(HoursPerWeek)
-boxplot(TotalHours)
-summary(skilldata)
-which.max(skilldata$TotalHours)
-skilldata[1794,5] = 25000
-mean(TotalHours,na.rm = T)
-which.max(TotalHours)
-skilldata[1794,5]
-TotalHours
-TotalHours[1794] = 25000
-plot(HoursPerWeek)
-24*7
 
 
-#Age
-# 큰 차이는 없음 프로선수 데이터 활용 채워 넣기
+# Age
+boxplot(Age) # 이상값 다수 존재 ( 프로 선수 데이터는 설문을 통해 나이, 주당 시간, 총 시간을 구하지 않아 공백)
+#프로선수 나이 데이터를 바탕으로 Age의 프로 데이터 채워 넣기 
 pro <- c(21,	25,	21,	23,	26,	23,	26,	25,	23,	24,	25,	22,	22,	22,	25,	25,	23,	23,	24,	21,	25,	23,	22,	23,	24,	22,	20,	23,	23,	20,	23,	21,	25,	22,	25,	25,	25,	23,	20,	24,	25,	22,	25,	24,	17,	22,	22,	20,	27,	27,	24,	25,	27,	28,	19,	27,	19,	24,	27,	25,	21,	23,	23,	20,	24,	22,	23,	21,	20,	25)
+
 #HourPerWeek
 # 설문을 통한 데이터인듯
+hist(HoursPerWeek)
+boxplot(TotalHours)
+which.max(skilldata$TotalHours)
+skilldata[1794,5] = 25000
+
 #TotalHour
+TotalHours
+TotalHours[1794] = 25000
+which.max(TotalHours)
+skilldata[1794,5]
 # 설문을 통한 데이터인듯
+
 
 #AMP
 hist(APM)
@@ -98,8 +96,6 @@ lines(density(skilldata_8$SelectByHotkeys), col = mycol[8],lwd = 2)
 legend('topright',c('Bronze','Silver','Gold','Platinum','Diamond','Master','GrandMaster','Pro'),col = c('saddle brown','Azure 3','gold2','AQUAMARINE','plum','light sky blue',' royal blue','midnight blue'),pch=15)
 
 
-cor(LeagueIndex,SelectByHotkeys)
-summary(SelectByHotkeys) # 등급별 평균 확인하기
 
 #AssignToHotkeys
 str(skilldata$AssignToHotkeys)
@@ -281,7 +277,7 @@ legend('topright',c('Bronze','Silver','Gold','Platinum','Diamond','Master','Gran
 skilldata %>%
   group_by(LeagueIndex) %>%
   summarize(mean_TME = mean(TotalMapExplored, na.rm = TRUE))
-# 프로씬은 맵확인을 많이 하지 않음
+# pro는 맵확인을 많이 하지 않음
 
 #WorkersMade
 str(skilldata$WorkersMade)
@@ -368,15 +364,7 @@ which.max(HoursPerWeek)
 skilldata[691,]
 
 cor(GapBetweenPACs,LeagueIndex)
-#------------데이터 분류 ----------------------------
-skilldata_1 = skilldata[skilldata$LeagueIndex == 1,]
-skilldata_2 = skilldata[skilldata$LeagueIndex == 2,]
-skilldata_3 = skilldata[skilldata$LeagueIndex == 3,]
-skilldata_4 = skilldata[skilldata$LeagueIndex == 4,]
-skilldata_5 = skilldata[skilldata$LeagueIndex == 5,]
-skilldata_6 = skilldata[skilldata$LeagueIndex == 6,]
-skilldata_7 = skilldata[skilldata$LeagueIndex == 7,]
-skilldata_8 = skilldata[skilldata$LeagueIndex == 8,]
+
 plot(LeagueIndex,TotalHours)
 summary(skilldata_1)
 summary(skilldata_2)
